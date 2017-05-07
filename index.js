3371acd2fdbaa1e4be4a8eac26291d059239763f
Jeff <jeff@imginconline.com>
2017-05-07 08:41:19 -0400

    Reworking redbird letsencrypt configuration

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
 var platform = process.env.NOINFOPATHDEBUG || "prod",
 	config = require("./config")[platform],
-	proxy = require('redbird')({
-		port: config.inboundPort,
-		ssl: config.ssl
-	});
+	proxy = require('redbird')(config.redbird.init);
 
 //Create proxy for each item in the config.json file.
-config.proxies.forEach(function (prx) {
-	proxy.register(prx.source, prx.dest, {ssl: true});
+config.redbird.proxies.forEach(function (prx) {
+	proxy.register(prx.source, prx.dest, prx.options);
 });
-
-
-
-// LetsEncrypt requires a minimal web server for handling the challenges, this is by default on port 3000
-// it can be configured when initiating the proxy. This web server is only used by Redbird internally so most of the time
-// you  do not need to do anything special other than avoid having other web services in the same host running
-// on the same port.
-
-//
-// HTTP2 Support using LetsEncrypt for the certificates
-//
-// var proxy = require('redbird')({
-// 	letsencrypt: {
-// 		path: __dirname + '/certs',
-// 		port: 9999
-// 	},
-// 	ssl: {
-// 		http2: true,
-// 	}
-// });
