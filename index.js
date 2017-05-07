440ddc7977b4b5f0a5dcc8fc2f31f1ecdd38fba6
Jeff <jeff@imginconline.com>
2017-05-07 11:35:04 -0400

    Fixed configuration.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var platform = process.env.NOINFOPATHDEBUG || "prod",
 	config = require("./config")[platform],
 	proxy = require('redbird')(config.redbird.init);
 
+console.log("Start NoInfoPath Reverse Proxy in ", platform);
+
 //Create proxy for each item in the config.json file.
 config.redbird.proxies.forEach(function (prx) {
 	proxy.register(prx.source, prx.dest, prx.options);
