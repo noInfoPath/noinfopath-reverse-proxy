84d54f468fdea5df45f15b63edbd0e528198ed17
Jeff <jeff@imginconline.com>
2017-09-13 21:11:05 -0400

    Modified to take a command line argument --config.  Can be a URL or file path.

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-var platform = process.env.NOINFOPATHDEBUG || "prod",
-	config = require("./config")[platform],
-	proxy = require('redbird')(config.redbird.init);
+var request = require("request-promise"),
+	fs = require("fs"),
+	args = require('minimist')(process.argv.slice(2)),
+	proxy
+	;
 
-console.log("Start NoInfoPath Reverse Proxy in ", platform);
+//console.log("Start NoInfoPath Reverse Proxy in ", platform);
+
+function resloveConfig(path) {
+	if(path.indexOf("http") === 0) {
+		return request(path)
+			.then(function(results){
+				return JSON.parse(results);
+			})
+			.catch(function(err){
+				throw err;
+			});
+	} else {
+		console.log("TODO: Handle local file system.");
+		return Promise.resolve({redbird: {proxies: []}})
+	}
+}
+
+resloveConfig(args.config)
+	.then(function(config){
+		proxy = require('redbird')(config.redbird.init);
+
+		config.redbird.proxies.forEach(function (prx) {
+			proxy.register(prx.source, prx.dest, prx.options);
+		});
+	})
+	.catch(function(err){
+		console.error(err);
+	});
+
 
 //Create proxy for each item in the config.json file.
-config.redbird.proxies.forEach(function (prx) {
-	proxy.register(prx.source, prx.dest, prx.options);
-});
